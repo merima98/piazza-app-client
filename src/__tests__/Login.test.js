aae74d4bd50c8317d8989145d8c9fa1ae4b40b44
merima98 <merimaceranic98@gmail.com>
2022-04-18 15:08:02 +0200

    Added unit test for login form.

---
+++ src/__tests__/Login.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter as Router } from "react-router-dom";
+import Login from "../features/login/Login";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "../reducer";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+test("Should render Login component.", () => {
+  render(
+    <Provider store={store}>
+      <Router>
+        <Login />
+      </Router>
+    </Provider>
+  );
+
+  expect(screen.getByRole("button", { name: /Login/i })).toBeEnabled();
+});
